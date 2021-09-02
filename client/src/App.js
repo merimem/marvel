509ff545568b7445d8a1d2a814dc0ce125a298ef
meriem memmiche <meriemmemmiche@gmail.com>
2021-09-02 14:47:19 +0200

    fix css cards

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 
 import Navbar from "./components/layout/Navbar"
 import AllCharacters from "./components/AllCharacters"
+import './App.css'
 function App() {
   return (
     <div className="App">
