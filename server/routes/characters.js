d11440bbd7bc8aa89fa78d40b43eee1446624d0b
meriem memmiche <meriemmemmiche@gmail.com>
2021-09-02 15:06:07 +0200

    fix apikey

--- server/routes/characters.js
+++ server/routes/characters.js
@@ -9,7 +9,7 @@ router.get('/:offset', function(req, res, next) {
       uri: 'http://gateway.marvel.com/v1/public/characters',
       qs: {
         ts: 1,
-        apikey: process.env.REACT_APP_MARVEL_KEY,
+        apikey: "78b5dfec6c288aeeb34e2ff73255a539",
         hash: 'ab6c308a0a27bb3a1823723dc56900cb',
         limit: 5,
         offset: req.params.offset
